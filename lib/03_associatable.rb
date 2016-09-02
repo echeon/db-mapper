70b8da2575b1b65b589d43c428574d24506951fd
Euibin Cheon <euibinc@gmail.com>
2016-09-02 17:23:21 -0400

    Add has_one_through

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -39,6 +39,7 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
     define_method(name) do
       foreign_key_value = self.send(options.foreign_key)
       target_model_class = options.model_class
@@ -59,7 +60,7 @@ module Associatable
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
